04528a9f689f52b4a81656db58d12b533453b175
sseyma-53 <sseyma5317@gmail.com>
2025-09-19 03:23:38 +0000

    coded for payroll data on employees

--- main.cpp
+++ main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main()
+{
+
+ifstream inData;
+string fileName;
+string firstName, lastName;
+double baseSalary, commissionPercent, sales, expenses, grossPay, commission;
+
+
+cout << "Please enter the name of the input file." << endl;
+cin >> fileName;
+inData.open(fileName);
+
+
+
+inData >> firstName >> lastName;
+inData >> baseSalary >> commissionPercent;
+inData >> sales;
+inData >> expenses;
+
+commission = sales*(commissionPercent/100);
+grossPay = baseSalary + commission + expenses;
+
+cout << "Payroll Data for " << firstName << " " << lastName << endl;
+cout << endl;
+cout << setprecision(2) << fixed;
+
+
+cout << "Base Salary: " << setw(10) << right << baseSalary << endl; 
+cout << "Commission: " << setw(11) << right << commission << setprecision(2) << " (" << commissionPercent << "% of " << sales << ")" << endl;
+cout << "Expenses: " << setw(13) << right << expenses << setprecision(2) << endl;
+cout << setw(23) << "--------" << right << endl << "Total: " << setw(15) << grossPay;
 }
+  
